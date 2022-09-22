738d067382a4ef3c518524103bc76aa8a59a739c
Gurazeez Singh Sachdeva <66404378+gurazeez07@users.noreply.github.com>
2022-09-22 01:00:50 -0400

    Create connectionAtomGenerator.ex

---
+++ lib/connectionAtomGenerator.ex
@@ -0,0 +1,52 @@
+defmodule PROJECT1.ConnectionAtomGenerator do
+
+  #this randomizer function for generating random random atom for connexting to the server
+  # is taken from https://gist.github.com/ahmadshah/8d978bbc550128cca12dd917a09ddfb7
+    
+    @moduledoc """
+  Random string generator module.
+  """
+  
+  @doc """
+  Generate random string based on the given legth. It is also possible to generate certain type of randomise string using the options below:
+  * :all - generate alphanumeric random string
+  * :alpha - generate nom-numeric random string
+  * :numeric - generate numeric random string
+  * :upcase - generate upper case non-numeric random string
+  * :downcase - generate lower case non-numeric random string
+  ## Example
+      iex> Iurban.String.randomizer(20) //"Je5QaLj982f0Meb0ZBSK"
+  """
+  def randomizer(length, type \\ :all) do
+    alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+
+    lists =
+      cond do
+        type == :alpha -> alphabets <> String.downcase(alphabets)
+        type == :numeric -> numbers
+        type == :upcase -> alphabets
+        type == :downcase -> String.downcase(alphabets)
+        true -> alphabets <> String.downcase(alphabets) <> numbers
+      end
+      |> String.split("", trim: true)
+    do_randomizer(length, lists)
+  end
+
+  @doc false
+  defp get_range(length) when length > 1, do: (1..length)
+  defp get_range(length), do: [1]
+
+  @doc false
+  defp do_randomizer(length, lists) do
+    get_range(length)
+    |> Enum.reduce([], fn(_, acc) -> [Enum.random(lists) | acc] end)
+    |> Enum.join("")
+  end
+
+  #generate the input atom
+  def get_connection_atom(meraIP) do
+    m=String.to_atom(randomizer(5,:all)<>"@"<>meraIP)
+    m
+  end     
+end
